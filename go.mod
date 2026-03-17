d418c7693170aa4b97c56428fe3df055340aa856
y29-h <hryb290809@gmail.com>
2026-03-18 00:01:30 +0200

    init go module and connect database

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/y29-h/Formgram
+
+go 1.25.0
+
+require github.com/mattn/go-sqlite3 v1.14.37 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/y29-h/Formgram/db"
 )
 
 func main() {
+	db.Init()
+
 	mux := http.NewServeMux()
 
 	log.Println("Server started on :8080")
